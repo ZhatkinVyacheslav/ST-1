2028c3266bd33dea9266874dca1cda36f626fdc9
ZhatkinVyacheslav <zhatkins1009@mail.ru>
2024-04-09 14:13:37 +0300

    1st Task

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,44 @@
-// Copyright 2022 NNTU-CS
+// Copyright Zhatkin Vyacheslav
 #include <cstdint>
 #include "alg.h"
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if (value <= 1) return false;
+    if (value <= 3) return true;
+    if (value % 2 == 0 || value % 3 == 0) return false;
+    for (uint64_t i = 5; i * i <= value; i += 6) {
+        if (value % i == 0 || value % (i + 2) == 0) return false;
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    uint64_t count = 0;
+    uint64_t num = 2;
+    while (count < n) {
+        if (checkPrime(num)) {
+            count++;
+        }
+        num++;
+    }
+    return num - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    value++;
+    while (!checkPrime(value)) {
+        value++;
+    }
+    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+    return sum;
 }
